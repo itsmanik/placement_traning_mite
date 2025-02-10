e06648239bf1b8bf558f447f9d58576faa78a576
itsmanik <manik.hilalpure@gmail.com>
2025-02-10 22:40:48 +0530

    done with ass 2 question 3

--- assignment_3/question_3.cpp
+++ assignment_3/question_3.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 int main() {
     vector<int> arr = { 5, 10, 3, 7, 8 };
-    vector<int> postfixSum;
     int sum = 0;
-    
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        sum += arr[i];
+        arr[i] = sum;
+    }
+    for (auto& item : arr) cout << item << " ";
     return 0;
 }
